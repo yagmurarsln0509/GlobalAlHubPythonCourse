9dd6214015abcafb4786c93a8716bc5698b7e966
Yağmur Arslan <76778474+yagmurarsln0509@users.noreply.github.com>
2021-04-07 17:27:09 +0300

    Create Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,18 @@
+#homeworkpart1
+my_list = [2,3,5,7,6,1,4,9,0,3]
+def swapHalf(my_list):
+    l = int(len(my_list)/2)
+    my_list[:l], my_list[l:] = my_list[l:], my_list[:l] 
+    print(my_list) 
+swapHalf(my_list)
+
+#homeworkpart2
+def evenNumbers(): 
+    while  True :
+        n = int(input('Bir tek basamaklı sayı giriniz'))
+        if n <10 and n>0 :
+            break
+    for i in range (0, n):
+        if i %2 == 0:
+            print(i)
+evenNumbers()
